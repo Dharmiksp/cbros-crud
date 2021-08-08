7276ff1787d99f20e24b3e7492db84b3d392fdbc
Dharmik <76110036+Dharmiksp@users.noreply.github.com>
2021-08-08 08:29:02 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 var mysqlConnection = mysql.createConnection({
-    host: "localhost",
-    user: "admin",
-    database: "cbros",
-    password: "Mission@2020",
+    host: "*****",
+    user: "*****",
+    database: "****",
+    password: "******",
     multipleStatements: true
 });
 
